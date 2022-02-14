144bc963c38e7fab4fa0d4ba7b332f8425babace
Jack Hodgkinson <30505104+jhodgdev@users.noreply.github.com>
2022-02-14 14:18:49 +0000

    Implement nix run

--- .github/format.sh
+++ .github/format.sh
@@ -3,4 +3,4 @@
 # Extensions necessary to tell fourmolu about 
 EXTENSIONS="-o -XTypeApplications -o -XTemplateHaskell -o -XImportQualifiedPost -o -XPatternSynonyms -o -fplugin=RecordDotPreprocessor"
 SOURCES=$(git ls-tree -r HEAD --full-tree --name-only | grep -E '.*\.hs')
-~/.local/bin/fourmolu --mode check --check-idempotence $EXTENSIONS $SOURCES
+nix run nixpkgs#haskellPackages.fourmolu -- --mode check --check-idempotence $EXTENSIONS $SOURCES
