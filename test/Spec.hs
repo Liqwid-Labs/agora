4cc3a5f7924dc0f88a2d1cb9d2c0d932091d609d
Jack Hodgkinson <30505104+jhodgdev@users.noreply.github.com>
2022-02-14 12:11:34 +0000

    Added failing test

--- test/Spec.hs
+++ test/Spec.hs
@@ -5,8 +5,14 @@ import Prelude
 --------------------------------------------------------------------------------
 
 import Test.Tasty (defaultMain, testGroup)
+import Test.Tasty.HUnit (assertBool, testCase)
 
 --------------------------------------------------------------------------------
 
 main :: IO ()
-main = defaultMain $ testGroup "Suites" []
+main =
+  defaultMain $
+    testGroup
+      "Suites"
+      [ testCase "will fail" $ assertBool "false" False
+      ]
