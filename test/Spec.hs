75187895bc7ad8c779a50890a7d4d4db94d5d8b6
Jack Hodgkinson <30505104+jhodgdev@users.noreply.github.com>
2022-02-14 13:43:35 +0000

    Examine behaviour of hlint and fourmolu without Stack

--- test/Spec.hs
+++ test/Spec.hs
@@ -5,14 +5,9 @@ import Prelude
 --------------------------------------------------------------------------------
 
 import Test.Tasty (defaultMain, testGroup)
-import Test.Tasty.HUnit (assertBool, testCase)
 
 --------------------------------------------------------------------------------
 
 main :: IO ()
 main =
-  defaultMain $
-    testGroup
-      "Suites"
-      [ testCase "will fail" $ assertBool "false" False
-      ]
+  defaultMain $ testGroup "Suites" []
