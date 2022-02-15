8f5decb73884036580813552c3ef4343990f34bf
Emily Martins <emi@haskell.fyi>
2022-02-16 00:33:14 +0100

    use GHC 9.2.1 fourmolu package in format.sh

--- .github/format.sh
+++ .github/format.sh
@@ -3,4 +3,4 @@
 # Extensions necessary to tell fourmolu about
 EXTENSIONS="-o -XTypeApplications -o -XTemplateHaskell -o -XImportQualifiedPost -o -XPatternSynonyms -o -XOverloadedRecordDot"
 SOURCES=$(git ls-tree -r HEAD --full-tree --name-only | grep -E '.*\.hs')
-nix run nixpkgs#haskellPackages.fourmolu -- --mode check --check-idempotence $EXTENSIONS $SOURCES
+nix run nixpkgs#haskell.packages.ghc921.fourmolu -- --mode check --check-idempotence $EXTENSIONS $SOURCES
