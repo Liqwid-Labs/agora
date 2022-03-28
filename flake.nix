2e312599e5ad403f040d98124b10355c0460c53c
Jack Hodgkinson <30505104+jhodgdev@users.noreply.github.com>
2022-03-28 14:20:34 +0100

    Bumped apropos

--- flake.nix
+++ flake.nix
@@ -13,9 +13,12 @@
 
   # https://github.com/mlabs-haskell/apropos-tx/pull/28
   inputs.apropos-tx.url =
-    "github:mlabs-haskell/apropos-tx?rev=5b74ba897a6f02718c163bf588a08c5e3e9de204";
+    "github:jhodgdev/apropos-tx?rev=582496d0dfb88ce007bb0d2a2dcbc72ea0bb1cd1";
   inputs.apropos-tx.inputs.nixpkgs.follows =
     "plutarch/haskell-nix/nixpkgs-unstable";
+
+  inputs.apropos.url =
+    "github:jhodgdev/apropos?rev=c6c580aeab8b5c2a6512a49823dd17936e87b70a";
 
   outputs = inputs@{ self, nixpkgs, haskell-nix, plutarch, ... }:
     let
@@ -55,6 +58,10 @@
               src = inputs.apropos-tx;
               subdirs = [ "." ];
             }
+            {
+              src = inputs.apropos;
+              subdirs = [ "." ];
+            }
           ];
           modules = [ (plutarch.haskellModule system) ];
           shell = {
@@ -85,6 +92,7 @@
               ps.plutarch-extra
               ps.plutarch-numeric
               ps.plutarch-test
+              ps.apropos
             ];
           };
         };
