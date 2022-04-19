5584a47528e3b385a697877a1d81c73f7ba9adbd
Seungheon Oh <seungheon.ooh@gmail.com>
2022-04-18 19:17:36 -0500

    format...

--- agora-test/Spec/Effect/TreasuryWithdrawalEffect.hs
+++ agora-test/Spec/Effect/TreasuryWithdrawalEffect.hs
@@ -5,13 +5,12 @@ Description: Sample based testing for Treasury Withdrawal Effect
 
 This module tests the Treasury Withdrawal Effect.
 -}
-
-module Spec.Effect.TreasuryWithdrawalEffect(currSymbol, signer, validator, validatorHashTN, withdrawalEffect) where
+module Spec.Effect.TreasuryWithdrawalEffect (currSymbol, signer, validator, validatorHashTN, withdrawalEffect) where
 
 import Plutarch.Api.V1
 import Plutus.V1.Ledger.Api
-import Plutus.V1.Ledger.Value qualified as Value
 import Plutus.V1.Ledger.Interval qualified as Interval
+import Plutus.V1.Ledger.Value qualified as Value
 
 import Agora.Effect.TreasuryWithdrawal (treasuryWithdrawalValidator)
 
@@ -34,26 +33,26 @@ validatorHashTN = let ValidatorHash vh = validatorHash validator in TokenName vh
 withdrawalEffect :: ScriptContext
 withdrawalEffect =
   ScriptContext
-    { scriptContextTxInfo  =
+    { scriptContextTxInfo =
         TxInfo
-              { txInfoInputs =
-                  [ TxInInfo
-                      (TxOutRef "0b2086cbf8b6900f8cb65e012de4516cb66b5cb08a9aaba12a8b88be" 1)
-                      TxOut
-                        { txOutAddress = Address (ScriptCredential $ validatorHash validator) Nothing
-                        , txOutValue = Value.singleton currSymbol validatorHashTN 1
-                        , txOutDatumHash = Nothing
-                        }
-                  ]
-              , txInfoOutputs = []
-              , txInfoFee = Value.singleton "" "" 2
-              , txInfoMint = mempty
-              , txInfoDCert = []
-              , txInfoWdrl = []
-              , txInfoValidRange = Interval.always
-              , txInfoSignatories = [signer]
-              , txInfoData = []
-              , txInfoId = "0b2086cbf8b6900f8cb65e012de4516cb66b5cb08a9aaba12a8b88be"
-              }
+          { txInfoInputs =
+              [ TxInInfo
+                  (TxOutRef "0b2086cbf8b6900f8cb65e012de4516cb66b5cb08a9aaba12a8b88be" 1)
+                  TxOut
+                    { txOutAddress = Address (ScriptCredential $ validatorHash validator) Nothing
+                    , txOutValue = Value.singleton currSymbol validatorHashTN 1
+                    , txOutDatumHash = Nothing
+                    }
+              ]
+          , txInfoOutputs = []
+          , txInfoFee = Value.singleton "" "" 2
+          , txInfoMint = mempty
+          , txInfoDCert = []
+          , txInfoWdrl = []
+          , txInfoValidRange = Interval.always
+          , txInfoSignatories = [signer]
+          , txInfoData = []
+          , txInfoId = "0b2086cbf8b6900f8cb65e012de4516cb66b5cb08a9aaba12a8b88be"
+          }
     , scriptContextPurpose = Spending (TxOutRef "0b2086cbf8b6900f8cb65e012de4516cb66b5cb08a9aaba12a8b88be" 1)
     }
