2c74fc3d0fe836055472940b8a691899f61aa8aa
Seungheon Oh <seungheon.ooh@gmail.com>
2022-05-03 11:17:07 -0400

    more generators

--- agora-test/Spec/Sample/Sample.hs
+++ agora-test/Spec/Sample/Sample.hs
@@ -1,20 +1,57 @@
-module Spec.Sample.Sample () where
+module Spec.Sample.Sample
+  ( -- * Credentials
+    -- $credentials
+    genUserCredential
+  , genScriptCredential
+  , genCredential
+    -- * Values
+    -- $values
+  , genValue
+  , genAssetClass
+  , genAnyValue
+  ) where
 
 import Test.QuickCheck 
 import Plutus.V1.Ledger.Api
+import Plutus.V1.Ledger.Value
+
+import Control.Applicative
 
 import Data.ByteString.Char8 qualified as C
 import Data.ByteString.Hash (sha2)
 
--- TODO: How do I need to ensure uniqueness?
-usersGen :: Gen Credential
-usersGen =
-  (chooseAny :: Gen Integer) >>= pure . PubKeyCredential . PubKeyHash . toBuiltin . sha2 . C.pack . show
+genHashByteString :: Gen C.ByteString
+genHashByteString = sha2 . C.pack . show <$> (chooseAny :: Gen Integer)
 
-treasuriesGen :: Gen Credential
-treasuriesGen =
-  (chooseAny :: Gen Integer) >>= pure . ScriptCredential . ValidatorHash . toBuiltin . sha2 . C.pack . show
+-- TODO: How do I need to ensure uniqueness?
+genUserCredential :: Gen Credential
+genUserCredential = PubKeyCredential . PubKeyHash . toBuiltin <$> genHashByteString
+
+genScriptCredential :: Gen Credential
+genScriptCredential = ScriptCredential . ValidatorHash . toBuiltin <$> genHashByteString
+
+genCredential :: Gen Credential
+genCredential = oneof [genUserCredential, genScriptCredential]
+
+genValue :: AssetClass -> Gen Value
+genValue ac = assetClassValue ac . abs <$> (chooseAny :: Gen Integer)
+
+genAssetClass :: Gen AssetClass
+genAssetClass = liftA2 assetClass (currencySymbol <$> genHashByteString) (tokenName <$> genHashByteString)
+
+genAnyValue :: Gen Value
+genAnyValue = genAssetClass >>= genValue
 
 txInInfoGen :: Gen TxInInfo
 txInInfoGen = do
   undefined
+
+{- | $credentials
+Generators for Plutus Credentials. These will generate
+randomized credential.
+-}
+
+{- | $values
+Generators for Plutus Values and AssetClass. These will generate
+randomized Values and AssetClass.
+-}
