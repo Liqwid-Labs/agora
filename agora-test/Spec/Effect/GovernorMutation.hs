30b66a29ffb3a215b92f4769b535fa0516772ec5
fanghr <chfanghr@gmail.com>
2022-05-07 18:49:37 +0800

    add a simple test

--- agora-test/Spec/Effect/GovernorMutation.hs
+++ agora-test/Spec/Effect/GovernorMutation.hs
@@ -1,6 +1,30 @@
 module Spec.Effect.GovernorMutation (tests) where
 
-import Test.Tasty (TestTree)
+import Agora.Effect.GovernorMutation (MutateGovernorDatum (..), mutateGovernorValidator)
+import Agora.Governor (GovernorDatum (..))
+import Agora.Proposal (ProposalId (..))
+import Plutus.V1.Ledger.Api (TxOutRef (..))
+import Spec.Sample.Effect.GovernorMutation
+import Spec.Sample.Shared
+import Spec.Util (effectSucceedsWith)
+import Test.Tasty (TestTree, testGroup)
 
 tests :: [TestTree]
-tests = []
+tests =
+  [ testGroup
+      "validator"
+      [ effectSucceedsWith
+          "Simple"
+          (mutateGovernorValidator governor)
+          ( MutateGovernorDatum
+              { governorRef = TxOutRef "614481d2159bfb72350222d61fce17e548e0fc00e5a1f841ff1837c431346ce7" 1
+              , newDatum =
+                  GovernorDatum
+                    { nextProposalId = ProposalId 42
+                    , proposalThresholds = defaultProposalThresholds
+                    }
+              }
+          )
+          validContext
+      ]
+  ]
